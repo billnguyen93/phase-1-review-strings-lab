371f87cdf0da21de409bc9ffd0299fc50f9852be
Bill Nguyen <billfl93@gmail.com>
2022-07-20 17:49:48 -0400

    commit tree 7657aa55e2c16518243c10ee2620bb61d14b25cd

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Bill Nguyen';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+
+
+const firstInitial = currentUser[0];
+const shortGreeting = `Welcome, ${firstInitial}!`;
